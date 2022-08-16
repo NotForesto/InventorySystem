fc2910863bd9a608c20e38498622dd0ec432d53c
Forest Burchinal-Haj <forest.burchinal@icloud.com>
2022-08-16 12:11:58 -0700

    Added methods to change scene on stage when the "Add" buttons are pressed in either the parts or product section of the main form in MainScreen.fxml

--- src/main/java/forest/inventorysystem/controller/AddPartController.java
+++ src/main/java/forest/inventorysystem/controller/AddPartController.java
@@ -1,4 +1,21 @@
 package forest.inventorysystem.controller;
 
+import forest.inventorysystem.InventorySystem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddPartController {
+    public void returnToMain(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InventorySystem.class.getResource("MainScreen.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Add Parts");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/forest/inventorysystem/controller/MainScreenController.java
+++ src/main/java/forest/inventorysystem/controller/MainScreenController.java
@@ -1,17 +1,47 @@
 package forest.inventorysystem.controller;
 
+import forest.inventorysystem.InventorySystem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class MainScreenController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements Initializable {
     @FXML
     private Label welcomeText;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
 
-    @FXML
-    public void toAddPart(ActionEvent actionEvent) {
-        System.out.println("Clicked");
+    // Loads the AddPart form when the "Add" button is pressed in the part section of the MainScreen form.
+    public void toAddPart(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InventorySystem.class.getResource("AddPart.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Add Parts");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Loads the AddProduct form when the "Add" button is pressed in the product section of the MainScreen form.
+    public void toAddProduct(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(InventorySystem.class.getResource("AddProduct.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Add Product");
+        stage.setScene(scene);
+        stage.show();
     }
 }
